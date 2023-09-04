9ac99fa100a0cf90ddcb55b68d79fd0ee20f0efd
Zodiac132 <100285202+Zodiac132@users.noreply.github.com>
2023-09-04 01:02:21 -0400

    Update kdkiadkwd.lua

--- kdkiadkwd.lua
+++ kdkiadkwd.lua
@@ -1,3 +1,3 @@
-g getgenv().HostUser = "ZodiacOnStreaks"
+getgenv().HostUser = "ZodiacOnStreaks"
 getgenv().Alts = {750082213, 334839045, 239501455, 2788908688, 241180828, 358785984, 341416326, 425851394, 247333067, 1853010166, 2740119808, 2740095167, 235585779, 237157950, 722501131, 805792598, 723526727, 237136566, 722743366, 441607240, 808638805, 447525467, 723594514, 722270547, 722475735} --make sure to put space after every comma as shown in example
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Msorky/Duckys-return/main/Aura%22%5D%20%3D', true))()
